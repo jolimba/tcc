a8499405ad773a5d2963eeb2374bbc4412ec7b2b
jolimba <joovictors85@gmail.com>
2022-07-07 16:14:05 -0300

    segundo commmit

---
+++ src/card/CardItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Card.css';
+import Card from 'react-bootstrap/Card';
+
+const CardItem = () => {
+    return (
+        <Card className="Card-container">
+            <Card.Header>SDFDSF</Card.Header>
+            <Card.Body>
+                <Card.Title> Card Title </Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
+    );
+}
+
+export default CardItem
